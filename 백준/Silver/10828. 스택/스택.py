cef8727f8ec9f54d3110140d2a8d07fdc2655aee
Lee GaEun <137785327+kkeunii@users.noreply.github.com>
2024-07-31 12:04:10 +0900

    [Silver IV] Title: 스택, Time: 52 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,42 @@
+import sys
+
+result = []
+
+def push(n):
+    result.append(n)
+
+def pop():
+    if len(result) == 0:
+        return(-1)
+    else:
+        return result.pop()
+
+def size():
+    return len(result)
+
+def empty():
+    if len(result) == 0:
+        return(1)
+    else:
+        return(0)
+
+def top():
+    if len(result) == 0:
+        return(-1)
+    else:
+        return result[-1]
+
+count = int(sys.stdin.readline())
+
+for _ in range(count):
+    order = sys.stdin.readline().split()
+    if order[0] == 'push':
+        push(int(order[1]))
+    elif order[0] == 'pop':
+        print(pop())
+    elif order[0] == 'size':
+        print(size())
+    elif order[0] == 'empty':
+        print(empty())
+    elif order[0] == 'top':
+        print(top())
